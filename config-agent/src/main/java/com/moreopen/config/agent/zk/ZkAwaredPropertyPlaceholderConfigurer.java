c211f2821dd73288f4f05493d062597be213cae8
yekai2 <yekai2@lenovo.com>
2015-03-23 18:11:12 +0800

    xxx

--- config-agent/src/main/java/com/moreopen/config/agent/zk/ZkAwaredPropertyPlaceholderConfigurer.java
+++ config-agent/src/main/java/com/moreopen/config/agent/zk/ZkAwaredPropertyPlaceholderConfigurer.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues.ValueHolder;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.config.TypedStringValue;
 
@@ -51,6 +52,18 @@ public class ZkAwaredPropertyPlaceholderConfigurer extends PropertyPlaceholderCo
 					}
 				}
 			}
+			
+			//unsupport indexed constructor-arguments
+			List<ValueHolder> genericArgumentValues = beanDefinition.getConstructorArgumentValues().getGenericArgumentValues();
+			for (ValueHolder valueHolder : genericArgumentValues) {
+				if (TypedStringValue.class.isInstance(valueHolder.getValue())) {
+					String value = ((TypedStringValue) valueHolder.getValue()).getValue();
+					if (PlaceholderUtils.isPlaceholderProperty(value)) {
+						PropertyPlaceholderMethodWrappers.add(beanName, beanDefinition, valueHolder.getName(), PlaceholderUtils.trimPlaceholder(value));
+					}
+				}
+			}
+			
 		}
 		super.processProperties(beanFactoryToProcess, props);
 	}
