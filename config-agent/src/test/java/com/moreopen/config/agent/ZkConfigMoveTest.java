082adb9a731099ad08f25192753c2ff8506a36b7
yekai2 <yekai2@zenmen.com>
2016-12-27 15:17:40 +0800

    add zmchat assembly

---
+++ config-agent/src/test/java/com/moreopen/config/agent/ZkConfigMoveTest.java
@@ -0,0 +1,44 @@
+package com.moreopen.config.agent;
+
+import java.util.List;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZkConfigMoveTest {
+
+	public static void main(String[] args) throws Exception {
+		// 旧zk配置
+		ZooKeeper oldzk = new ZooKeeper("10.136.20.206:2181", 60000, null);
+		// 新zk配置
+		ZooKeeper newzk = new ZooKeeper("10.135.20.3:2181", 60000, null);
+		// 迁移的节点
+		String node = "/moconfig/dispatch-rs";
+		List<String> children = oldzk.getChildren(node, false);
+		move(oldzk, newzk, children, node);
+		oldzk.close();
+		newzk.close();
+	}
+
+	private static void move(ZooKeeper oldzk, ZooKeeper newzk, List<String> children, String parent)
+			throws KeeperException, InterruptedException {
+		if (children == null || children.isEmpty()) {
+			return;
+		} else {
+			for (String child : children) {
+				String c = parent + "/" + child;
+				System.out.println(c);
+				byte[] data = oldzk.getData(c, false, null);
+				if (newzk.exists(c, false) == null) {
+					newzk.create(c, data, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+				} else {
+					newzk.setData(c, data, -1, null, null);
+				}
+				move(oldzk, newzk, oldzk.getChildren(c, false), c);
+			}
+		}
+	}
+
+}
